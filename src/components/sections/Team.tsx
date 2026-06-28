fd3199c5ef410a4773d935cdc2acade3220bdf0e
preet893700 <aman@hekaos.com>
2026-06-29 01:57:03 +0530

    headhsot-updated

--- src/components/sections/Team.tsx
+++ src/components/sections/Team.tsx
@@ -35,7 +35,7 @@ export default function Team() {
       name: "Shwetaly",
       designation: "Trader & Mentor",
       imageSrc:
-        "https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "/Shwetaly-headshot.png",
       socialLinks: [
         { icon: InstagramIcon, href: "https://www.instagram.com/tradeiq.with.shwetaly?igsh=amxleDl4azFxeGJo" },
       ],
